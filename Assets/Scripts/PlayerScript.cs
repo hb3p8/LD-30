5f3ce7c989d827d49b59e81b3cdc0e7bc76aa95c
igor <dorofeyev07@gmail.com>
2014-08-23 21:39:09 +0400

    1st sprite

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	public Vector2 GravityVec;
+
+	public float Speed;
+
+	public float AngularSpeed;
+
+	public float MaxVelocity;
+
+	public float MaxAngularVelocity;
+
+	// Use this for initialization
+	void Start () {
+
+		GravityVec.x = 0.0f;
+		GravityVec.y = -0.2f;
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+	}
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		rigidbody2D.velocity += GravityVec;
+
+		//gameObject.transform.Rotate(0.0f,0.0f,-3.0f*moveHorizontal);
+
+		rigidbody2D.angularVelocity -= moveHorizontal;
+
+		Vector2 movement = new Vector2 (0.0f, moveVertical);
+		rigidbody2D.velocity += movement * Speed;
+	}
+}
